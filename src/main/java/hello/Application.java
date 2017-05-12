9f2bea37784e6ff8b42f37a381bda08eac0ad5e3
IPostevoi <baklandor410@yandex.ru>
2017-05-12 21:11:24 +0300

    2

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,16 +4,26 @@ package hello;
  * Created by bakla410 on 02.05.17.
  */
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Application.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
 
